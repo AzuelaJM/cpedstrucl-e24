8817c06a3b27aa5c10855e2fd06992dbd3041c63
John Mheynard Azuela <smegmash@gmail.com>
2016-12-17 11:14:37 +0800

    Add files via upload

---
+++ linked_list2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+
+class LinkedList{
+    struct Node {
+        int x;
+        Node *next;
+    };
+
+public:
+    LinkedList(){
+        head = NULL; // set head to NULL
+    }
+
+    void addValue(int val){
+        Node *n = new Node();   
+        n->x = val;             
+        n->next = head;         
+                               
+        head = n;              
+    }
+
+    int popValue(){
+        Node *n = head;
+        int ret = n->x;
+
+        head = head->next;
+        delete n;
+        return ret;
+        
+    }
+private:
+    Node *head; 
+};
+
+int main() {
+	int choice=0,nPush=0;
+    LinkedList list;
+    
+	while (1){
+	system ("cls");
+	
+	cout << "Menu" << endl
+		 << "==================" << endl
+		 <<"[1] Insert into Linked list" << endl
+		 <<"[2] Pop" << endl
+		 <<"[3] Exit" << endl
+		 << "==================" << endl
+		 <<"Choice: ";
+	cin >> choice; 
+	
+	switch (choice){
+		case 1:
+			int num;
+			cout << "Enter number: ";
+			cin >> num;
+			list.addValue(num);
+			nPush++;
+			break;
+			
+		case 2:
+			if(nPush==0)
+			{
+				cout << "Empty";
+			}
+			else{
+			cout << list.popValue();
+			nPush--;
+			}
+			getch();
+			break;
+			
+		case 3:
+			exit(1);		
+			
+		default:break;
+		
+	}	 
+		 
+}
+    
+    return 0;
+}
